abf44aa7e581c0ae7da003823221d469e9e8a88f
ishant005 <ishantyadav1996@gmail.com>
2021-11-04 17:07:20 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const inputs=document.querySelectorAll('.angle-input');
+const isTriangleBtn=document.querySelector('#is-triangle-btn');
+const outputE1=document.querySelector('#output');
+
+ function calculateSumofAngles(angle1,angle2,angle3){
+  const SumofAngles=angle1+angle2+angle3;
+  console.log(SumofAngles);
+
+
+ }
+
+function isTriangle() {
+    const SumofAngles=calculateSumofAngles(45,45,90);  
+}
+
+isTriangleBtn.addEventListener("click",isTriangle)
