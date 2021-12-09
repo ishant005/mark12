8012002aabc6c3fbb124df1bb14f4866088a2e2f
ishant005 <ishantyadav1996@gmail.com>
2021-12-09 18:53:20 +0530

    answer

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm=document.querySelector('.quiz-form');
 const submitAnswerBtn=document.querySelector("#submit-answer-btn")
 
 const outputEl=document.querySelector("#output");
-const correctAnswers=["90","right angled","acute","Scalene Triangle","Hypotenuse"];
+const correctAnswers=["90","right angled","less90","Scalene Triangle","Hypotenuse"];
 
 function calculateScore(){
      
